c297142e7403317ba2cf71a11b122114e8294547
Hardkore9t8 <nash3799@eagle.fgcu.edu>
2018-12-07 17:05:01 -0500

    Initial commit

---
+++ src/poshmark_selenium_automation/Main.java
@@ -0,0 +1,13 @@
+package poshmark_selenium_automation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Main {
+    public static void main(String[] args){
+System.setProperty("webdriver.gecko.driver",)
+        WebDriver obj =new FirefoxDriver();
+        obj.get("https://www.poshmark.com");
+
+    }
+}
